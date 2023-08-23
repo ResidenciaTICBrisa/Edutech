95e546453f0d21f51287eb488d0d14430392355b
luisgaboardi <luis.ggaboardi@gmail.com>
2023-08-22 22:46:09 -0300

    [Feat#16] - Criação do projeto FastAPI e React
    Co-authored-by: FauseSkyWalker <fausejr@gmail.com>

---
+++ src/frontend/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState({
+    id: 0,
+    content: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setMessage({ ...message, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/add_message",
+        message
+      );
+      console.log(response.data); // Mensagem de sucesso do back-end
+    } catch (error) {
+      console.error("Error adding message:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Add Message</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>ID:</label>
+          <input
+            type="number"
+            name="id"
+            value={message.id}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Content:</label>
+          <input
+            type="text"
+            name="content"
+            value={message.content}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Add Message</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
