d21e839b273b9585269651808b16232991eee194
FauseSkyWalker <Fausejr@gmail.com>
2023-09-22 15:45:10 -0300

    [US01] adicionando campos de cadstro e login

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,43 +2,102 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState({
-    id: 0,
-    content: "",
+  const [instituicao, setInstituicao] = useState({
+    nome: "",
+    endereco: "",
+    // Outros campos relevantes para o cadastro de instituições
   });
 
-  const handleInputChange = (event) => {
+  const [credenciaisLogin, setCredenciaisLogin] = useState({
+    nomeUsuario: "",
+    senha: "",
+  });
+
+  const handleInstituicaoInputChange = (event) => {
     const { name, value } = event.target;
-    setMessage({ ...message, [name]: value });
+    setInstituicao({ ...instituicao, [name]: value });
   };
 
-  const handleSubmit = async (event) => {
+  const handleLoginInputChange = (event) => {
+    const { name, value } = event.target;
+    setCredenciaisLogin({ ...credenciaisLogin, [name]: value });
+  };
+
+  const handleInstituicaoSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Enviar os dados da instituição para o servidor
       const response = await axios.post(
-        "http://localhost:8000/add_message",
-        message
+        "http://localhost:8000/cadastrar_instituicao",
+        instituicao
       );
-      console.log(response.data); // Mensagem de sucesso do back-end
+      console.log(response.data); // Mensagem de sucesso do servidor
     } catch (error) {
-      console.error("Error adding message:", error);
+      console.error("Erro ao cadastrar instituição:", error);
+    }
+  };
+
+  const handleInstituicaoLogin = async (event) => {
+    event.preventDefault();
+    try {
+      // Enviar as credenciais de login para o servidor
+      const response = await axios.post(
+        "http://localhost:8000/login_instituicao",
+        credenciaisLogin
+      );
+      console.log(response.data); // Mensagem de sucesso do servidor ou token de autenticação
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
     }
   };
 
   return (
     <div className="App">
-      <h1>Add Message</h1>
-      <form onSubmit={handleSubmit}>
+      <h2>Cadastrar Instituição</h2>
+      <form onSubmit={handleInstituicaoSubmit}>
         <div>
-          <label>Content:</label>
+          <label>Nome:</label>
           <input
             type="text"
-            name="content"
-            value={message.content}
-            onChange={handleInputChange}
+            name="nome"
+            value={instituicao.nome}
+            onChange={handleInstituicaoInputChange}
           />
         </div>
-        <button type="submit">Add Message</button>
+        <div>
+          <label>Endereço:</label>
+          <input
+            type="text"
+            name="endereco"
+            value={instituicao.endereco}
+            onChange={handleInstituicaoInputChange}
+          />
+        </div>
+        {/* Outros campos do formulário de cadastro de instituições */}
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      <h2>Login da Instituição</h2>
+      <form onSubmit={handleInstituicaoLogin}>
+        <div>
+          <label>Nome de Usuário:</label>
+          <input
+            type="text"
+            name="nomeUsuario"
+            value={credenciaisLogin.nomeUsuario}
+            onChange={handleLoginInputChange}
+          />
+        </div>
+        <div>
+          <label>Senha:</label>
+          <input
+            type="password"
+            name="senha"
+            value={credenciaisLogin.senha}
+            onChange={handleLoginInputChange}
+          />
+        </div>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
