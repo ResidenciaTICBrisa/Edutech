f1def194d8457dbbac9783a2340245b94ec7e333
luisgaboardi <luis.ggaboardi@gmail.com>
2023-09-23 20:06:58 -0300

    Menu inicial
    Co-authored-by: MarianaPRio <marianapires00@gmail.com>

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,45 +1,92 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [message, setMessage] = useState({
-    id: 0,
-    content: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setMessage({ ...message, [name]: value });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/add_message",
-        message
-      );
-      console.log(response.data); // Mensagem de sucesso do back-end
-    } catch (error) {
-      console.error("Error adding message:", error);
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>Add Message</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Content:</label>
-          <input
-            type="text"
-            name="content"
-            value={message.content}
-            onChange={handleInputChange}
-          />
+    <div className="div-2">
+      <div className="overlap">
+        <img className="cerebro" alt="Cerebro" src="cerebro-1-1.png" />
+        <div className="text-wrapper-3">Edutech</div>
+        <div className="overlap-2">
+          <div className="text-wrapper-4">Olá, usuárioooo</div>
+          {/* <div className="text-wrapper-5">logout</div>
+            <Logout className="logout-instance" /> */}
         </div>
-        <button type="submit">Add Message</button>
-      </form>
+      </div>
+      <div className="overlap-3">
+        <div className="rectangle-2" />
+        <div className="text-wrapper-6">Predição</div>
+        <div className="rectangle-3" />
+        <div className="text-wrapper-7">Predição</div>
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group-2">
+            <div className="rectangle-4" />
+            <div className="text-wrapper-7">Predição</div>
+          </div>
+        </div>
+        <div className="overlap-4">
+          <div className="rectangle-2" />
+          <div className="text-wrapper-7">Consulta</div>
+          <div className="rectangle-3" />
+          <div className="text-wrapper-8">Consulta</div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <div className="rectangle-4" />
+              <div className="text-wrapper-8">Consulta</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-5">
+          <div className="rectangle-2" />
+          <div className="text-wrapper-7">Gráficos</div>
+          <div className="rectangle-3" />
+          <div className="text-wrapper-8">Gráficos</div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <div className="rectangle-4" />
+              <div className="text-wrapper-8">Gráficos</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-6">
+          <div className="rectangle-5" />
+          <div className="text-wrapper-9">Cadastro</div>
+          {/* <Link className="rectangle-6" to="#" /> */}
+          <div className="text-wrapper-10">Cadastro</div>
+        </div>
+        <div className="overlap-7">
+          <div className="rectangle-2" />
+          <div className="text-wrapper-11">Feedback</div>
+          <div className="rectangle-3" />
+          <div className="text-wrapper-12">Feedback</div>
+          <div className="overlap-wrapper">
+            <div className="overlap-group-2">
+              <div className="rectangle-4" />
+              <div className="text-wrapper-12">Feedback</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-8">
+          <div className="text-wrapper-13">Página de Cadastramento</div>
+          {/* <img className="excel" alt="Excel" src="excel-1.png" />
+          <img className="aluna" alt="Aluna" src="aluna-2-1.png" /> */}
+          <p className="p">
+            Nesta opção é possível cadastrar um novo aluno para sua base de
+            dados.
+          </p>
+          <p className="text-wrapper-14">
+            Nesta opção é possível importar uma base de dados pré-preenchida.
+          </p>
+          {/* <Component
+            className="component-13"
+            divClassName="design-component-instance-node"
+            overlapGroupClassName="component-instance"
+            rectangleClassName="component-13-instance"
+          />
+          <PropertyDefaultWrapper className="component-14" property1="default" /> */}
+        </div>
+      </div>
     </div>
   );
 }
