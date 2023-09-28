421ae6a8108e87558fb71e622300daae09564de4
luisgaboardi <luis.ggaboardi@gmail.com>
2023-09-28 17:09:50 -0300

    Cria página de cadastro de aluno

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,47 +1,15 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CadastroAlunosPage from './pages/CadastroAlunos';
 
-function App() {
-  const [message, setMessage] = useState({
-    id: 0,
-    content: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setMessage({ ...message, [name]: value });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/add_message",
-        message
-      );
-      console.log(response.data); // Mensagem de sucesso do back-end
-    } catch (error) {
-      console.error("Error adding message:", error);
-    }
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>Add Message</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Content:</label>
-          <input
-            type="text"
-            name="content"
-            value={message.content}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit">Add Message</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/alunos/cadastro" element={<CadastroAlunosPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/frontend/src/pages/CadastroAlunos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CadastroAlunosPage = () => {
+  return (
+    <div>
+      <h1>Cadastro de alunos</h1>
+      <div>blabla</div>
+    </div>
+  );
+};
+
+export default CadastroAlunosPage;
