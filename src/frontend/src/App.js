de8c27824eee6bfcd3b6215fe3dc8420e447b741
MarianaPRio <marianapires00@gmail.com>
2023-10-02 18:42:17 -0300

    Adicionando logica para funcionamento das rotas das pages do front

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import CadastroAlunosPage from "./pages/cadastro_alunos/CadastroAlunos";
-import Header from "./components/header/Header";
-
+import CadastroInstituicaoPage from "./pages/cadastro_intituicao/CadastroInstituicao";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home/Home"
 const App = () => {
   return (
-    <div>
-      <Header />
-      <CadastroAlunosPage />
-    </div>
+  <Router>
+     <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/cadastro/instituicao" element={<CadastroInstituicaoPage/>} />
+        <Route path="/cadastro/alunos" element={<CadastroAlunosPage/>} />
+     </Routes>
+  </Router>
   );
 };
 
