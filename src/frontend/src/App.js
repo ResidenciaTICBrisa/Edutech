41bd3efd6315780867147958dc683b92c7a2e904
MarianaPRio <marianapires00@gmail.com>
2023-10-03 12:53:46 -0300

    Adicionando rotas para cadastro de aluno e contados/saibamais

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import CadastroAlunosPage from "./pages/cadastro_alunos/CadastroAlunos";
-import CadastroInstituicaoPage from "./pages/cadastro_intituicao/CadastroInstituicao";
+import CadastroAlunos from "./pages/cadastro_alunos/CadastroAlunos";
+import CadastrarAlunos from "./pages/cadastro_alunos/CadastrarAluno";
+
+import CadastroInstituicao from "./pages/cadastro_intituicao/CadastroInstituicao";
 import CadastrarInstituicao from "./pages/cadastro_intituicao/CadastrarInstituicao";
 
 import Consulta from "./pages/consulta/Consulta"
@@ -14,14 +16,20 @@ import Grafico from "./pages/grafico/Grafico"
 import Feedback from "./pages/feedback/Feedback"
 
 import Home from "./pages/home/Home"
+
 const App = () => {
   return (
   <Router>
      <Routes>
         <Route path="/" exact element={<Home/>} />
-        <Route path="/cadastro/instituicao" element={<CadastroInstituicaoPage/>} />
+        <Route path="/saibamais" exact element={<Home/>} />
+        <Route path="/contato" exact element={<Home/>} />
+        
+        <Route path="/cadastro/instituicao" element={<CadastroInstituicao/>} />
         <Route path="/cadastro/instituicao/cadastrar" element={<CadastrarInstituicao/>} />
-        <Route path="/cadastro/alunos" element={<CadastroAlunosPage/>} />
+
+        <Route path="/cadastro/alunos" element={<CadastroAlunos/>} />
+        <Route path="/cadastro/alunos/cadastrar" element={<CadastrarAlunos/>} />
 
         <Route path="/consulta" element={<Consulta/>} />
         <Route path="/predicao" element={<Predicao/>} />
