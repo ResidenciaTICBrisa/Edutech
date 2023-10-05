533b5009a198a570f221947ada5df69f5e277738
luisgaboardi <luis.ggaboardi@gmail.com>
2023-10-05 14:55:24 -0300

    Consulta de aluno

--- src/frontend/src/pages/consulta/Consulta.js
+++ src/frontend/src/pages/consulta/Consulta.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Consulta.css";
+import PessoaService from "../../services/PessoaService";
 
-function Consulta() {
+const Consulta = () => {
+
+  const [dados, setDados] = React.useState([]);
+
+  useEffect(() => {
+    PessoaService.getAlunos().then((res) => {
+      setDados(res)
+    });
+  }, []);
+
+ 
+
   return (
     <>
       <div className="div-fundo-consulta">
-        <p className="text-cadastrar"></p>
+      <table>
+      <thead>
+        <tr>
+          <th>CPF</th>
+          <th>Matrícula</th>
+          <th>Nome</th>
+          <th>Gênero</th>
+          <th>Estado</th>
+          <th>Cidade</th>
+          <th>Bairro</th>
+          <th>CEP</th>
+          <th>Logradouro</th>
+          <th>Número</th>
+          <th>Complemento</th>
+          <th>Data de Nascimento</th>
+          <th>Acessa Internet</th>
+        </tr>
+      </thead>
+      <tbody>
+        {dados.map((pessoa, index) => (
+          <tr key={index}>
+            <td>{pessoa.cpf}</td>
+            <td>{pessoa.matricula}</td>
+            <td>{pessoa.nome}</td>
+            <td>{pessoa.genero}</td>
+            <td>{pessoa.siglaEstado}</td>
+            <td>{pessoa.cidade}</td>
+            <td>{pessoa.bairro}</td>
+            <td>{pessoa.cep}</td>
+            <td>{pessoa.logradouro}</td>
+            <td>{pessoa.numero}</td>
+            <td>{pessoa.complemento}</td>
+            <td>{pessoa.dataNascimento}</td>
+            <td>{pessoa.acessaInternet ? 'Sim' : 'Não'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
       </div>
     </>
   );
