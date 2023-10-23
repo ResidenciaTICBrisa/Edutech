68171b4b15090eaaa8803ebfa5903b089a7e2121
MarianaPRio <marianapires00@gmail.com>
2023-10-23 16:16:27 -0300

    Adicionando pagina de acesso a consultas e cadastros

--- src/frontend/src/pages/cadastro/Cadastro.js
+++ src/frontend/src/pages/cadastro/Cadastro.js
@@ -3,31 +3,119 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/header/Header";
 import "./Cadastro.css";
+import aluno from "../../img/aluno.png";
+import disciplina from "../../img/disciplina.png";
+import turma from "../../img/turma.png";
+import unidade from "../../img/unidade.png";
+import instituicao from "../../img/instituicao.png";
+import pensar from "../../img/pensar.png";
 
 function Cadastro() {
   return (
     <>
       <Header />
-      <div className="dark-background button-container">
-        <Link to="aluno">
-          <button className="styled-button">Cadastro de Aluno</button>
-        </Link>
+      <div className="div-fundo-cadastro">
+        <p className="text-cadastro-cadastro">O que deseja cadastrar?</p>
+        <div className="div-fundo-content">
+          <div className="child-cadastro-1">
+            <div className="img-cadastro-cadastro-content">
+              <img
+                className="img-cadastro-cadastro"
+                src={aluno}
+                alt="Aluno"
+              />
+            </div>
+            <Link to="aluno">
+              <div className="content-cadastro">
+                <button className="button-cadastro-cadastro">
+                Cadastrar Aluno
+                </button>
+              </div>
+            </Link>
+          </div>
 
-        <Link to="instituicao">
-          <button className="styled-button">Cadastro de Instituição</button>
-        </Link>
+          {/* <div className="img-cadastro-cadastro-content">
+              <img
+                className="img-cadastro-cadastro"
+                src={pensar}
+                alt="pensar"
+              />
+            </div> */}
 
-        <Link to="unidade">
-          <button className="styled-button">Cadastro de Unidade</button>
-        </Link>
+          <div className="child-cadastro-1">
+          <div className="img-cadastro-cadastro-content">
+              <img
+                className="img-cadastro-cadastro"
+                src={instituicao}
+                alt="Cadastrar Instituição"
+              />
+            </div>
+            <Link to="instituicao">
+              <div className="content-cadastro">
+                <button className="button-cadastro-cadastro">
+                  Cadastrar Instituição
+                </button>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="div-fundo-content">
 
-        <Link to="disciplina">
-          <button className="styled-button">Cadastro de Disciplina</button>
-        </Link>
+          <div className="child-cadastro">
+          <div className="img-cadastro-cadastro-content">
+              <img
+                className="img-cadastro-cadastro"
+                src={unidade}
+                alt="Cadastrar Unidade"
+              />
+            </div>
+            <Link to="unidade">
+              <div className="content-cadastro">
+                <button className="button-cadastro-cadastro">
+                  Cadastrar Unidade
+                </button>
+              </div>
+            </Link>
+          </div>
 
-        <Link to="turma">
-          <button className="styled-button">Cadastro de Turma</button>
-        </Link>
+          <div className="child-cadastro">
+          <div className="img-cadastro-cadastro-content">
+              <img
+                className="img-cadastro-cadastro"
+                src={disciplina}
+                alt="Cadastrar Disciplina"
+              />
+            </div>
+            <Link to="/cadastro/cadastro/novo">
+              <div className="content-cadastro">
+                <button className="button-cadastro-cadastro">
+                  Cadastrar Disciplina
+                </button>
+              </div>
+            </Link>
+          </div>
+
+          <div className="child-cadastro">
+          <div className="img-cadastro-cadastro-content">
+              <img
+                className="img-cadastro-cadastro"
+                src={turma}
+                alt="Cadastrar Instituição"
+              />
+            </div>
+            <Link to="turma">
+              <div className="content-cadastro">
+                <button className="button-cadastro-cadastro">
+                  Cadastrar Turma
+                </button>
+              </div>
+            </Link>
+          </div>
+        </div>
+        
+        <div><br/></div>
+        <div><br/></div>
+
       </div>
     </>
   );
--- src/frontend/src/pages/consulta/Consulta.js
+++ src/frontend/src/pages/consulta/Consulta.js
@@ -3,31 +3,119 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/header/Header";
 import "./Consulta.css";
+import aluno from "../../img/aluno.png";
+import disciplina from "../../img/disciplina.png";
+import turma from "../../img/turma.png";
+import unidade from "../../img/unidade.png";
+import instituicao from "../../img/instituicao.png";
+import pensar from "../../img/pensar.png";
 
 function Consulta() {
   return (
     <>
       <Header />
-      <div className="dark-background button-container">
-        <Link to="aluno">
-          <button className="styled-button">Consulta de Aluno</button>
-        </Link>
+      <div className="div-fundo-consulta">
+        <p className="text-consulta-consulta">O que deseja consultar?</p>
+        <div className="div-fundo-content">
+          <div className="child-consulta-1">
+            <div className="img-consulta-consulta-content">
+              <img
+                className="img-consulta-consulta"
+                src={aluno}
+                alt="Aluno"
+              />
+            </div>
+            <Link to="aluno">
+              <div className="content-consulta">
+                <button className="button-consulta-consulta">
+                Consultar Aluno
+                </button>
+              </div>
+            </Link>
+          </div>
 
-        <Link to="instituicao">
-          <button className="styled-button">Consulta de Instituição</button>
-        </Link>
+          {/* <div className="img-consulta-consulta-content">
+              <img
+                className="img-consulta-consulta"
+                src={pensar}
+                alt="pensar"
+              />
+            </div> */}
 
-        <Link to="unidade">
-          <button className="styled-button">Consulta de Unidade</button>
-        </Link>
+          <div className="child-consulta-1">
+          <div className="img-consulta-consulta-content">
+              <img
+                className="img-consulta-consulta"
+                src={instituicao}
+                alt="Consultar Instituição"
+              />
+            </div>
+            <Link to="instituicao">
+              <div className="content-consulta">
+                <button className="button-consulta-consulta">
+                  Consultar Instituição
+                </button>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="div-fundo-content">
 
-        <Link to="disciplina">
-          <button className="styled-button">Consulta de Disciplina</button>
-        </Link>
+          <div className="child-consulta">
+          <div className="img-consulta-consulta-content">
+              <img
+                className="img-consulta-consulta"
+                src={unidade}
+                alt="Consultar Unidade"
+              />
+            </div>
+            <Link to="unidade">
+              <div className="content-consulta">
+                <button className="button-consulta-consulta">
+                  Consultar Unidade
+                </button>
+              </div>
+            </Link>
+          </div>
 
-        <Link to="turma">
-          <button className="styled-button">Consulta de Turma</button>
-        </Link>
+          <div className="child-consulta">
+          <div className="img-consulta-consulta-content">
+              <img
+                className="img-consulta-consulta"
+                src={disciplina}
+                alt="Consultar Disciplina"
+              />
+            </div>
+            <Link to="/consulta/consulta/novo">
+              <div className="content-consulta">
+                <button className="button-consulta-consulta">
+                  Consultar Disciplina
+                </button>
+              </div>
+            </Link>
+          </div>
+
+          <div className="child-consulta">
+          <div className="img-consulta-consulta-content">
+              <img
+                className="img-consulta-consulta"
+                src={turma}
+                alt="Consultar Instituição"
+              />
+            </div>
+            <Link to="turma">
+              <div className="content-consulta">
+                <button className="button-consulta-consulta">
+                  Consultar Turma
+                </button>
+              </div>
+            </Link>
+          </div>
+        </div>
+        
+        <div><br/></div>
+        <div><br/></div>
+
       </div>
     </>
   );
